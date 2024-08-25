b650a80c8a98e19151d414c672e6d8fe42f0e954
Andrew <andrew.panix@gmail.com>
2024-08-25 19:37:09 +0300

    init

--- views/admin/job/_index-item.php
+++ views/admin/job/_index-item.php
@@ -12,23 +12,23 @@ $format = Yii::$app->formatter;
 $status = $model->getStatus();
 switch ($status) {
     case PushRecord::STATUS_STOPPED:
-        $statusClass = 'bg-info';
+        $statusClass = 'info';
         break;
     case PushRecord::STATUS_WAITING:
-        $statusClass = 'bg-info';
+        $statusClass = 'info';
         break;
     case PushRecord::STATUS_STARTED:
-        $statusClass = 'bg-primary';
+        $statusClass = 'primary';
         break;
     case PushRecord::STATUS_FAILED:
     case PushRecord::STATUS_RESTARTED:
-        $statusClass = 'bg-danger';
+        $statusClass = 'danger';
         break;
     case PushRecord::STATUS_BURIED:
-        $statusClass = 'bg-danger';
+        $statusClass = 'danger';
         break;
     default:
-        $statusClass = 'bg-success';
+        $statusClass = 'success';
 }
 ?>
 <div class="job-item d-none">
@@ -41,12 +41,12 @@ switch ($status) {
                     'sender' => $format->asText($model->sender_name)
                 ]) ?>
             </a>
-            <?php if ($model->parent): ?>
+            <?php if ($model->parent){ ?>
                 <?= Yii::t('queue-monitor/default', 'from') ?>
                 <a href="<?= Url::to(['view', 'id' => $model->parent->id]) ?>" data-pjax="0">
                     #<?= $format->asText($model->parent->job_uid) ?>
                 </a>
-            <?php endif ?>
+            <?php } ?>
         </div>
         <div class="job-push-time">
             <?= Yii::t('queue-monitor/default', 'Pushed') ?>: <?= $format->asDatetime($model->pushed_at) ?>
@@ -60,100 +60,107 @@ switch ($status) {
         <div class="job-exec-attempts" title="<?= Yii::t('queue-monitor/default', 'Number of attempts.') ?>">
             <?= Yii::t('queue-monitor/default', 'Attempts') ?>: <?= $format->asInteger($model->getAttemptCount()) ?>
         </div>
-        <div class="job-exec-wait-time" title="<?= Yii::t('queue-monitor/default', 'Waiting time from push till first execute.') ?>">
+        <div class="job-exec-wait-time"
+             title="<?= Yii::t('queue-monitor/default', 'Waiting time from push till first execute.') ?>">
             <?= Yii::t('queue-monitor/default', 'Wait') ?>: <?= $format->asInteger($model->getWaitTime()) ?>s
         </div>
-        <?php if ($model->lastExec): ?>
-            <div class="job-exec-time" title="<?= Yii::t('queue-monitor/default', 'Last execute time and memory usage.') ?>">
-                <?= Yii::t('queue-monitor/default', 'Exec') ?>: <?= $format->asInteger($model->lastExec->getDuration()) ?>s
-                <?php if ($model->lastExec->memory_usage): ?>
+        <?php if ($model->lastExec){ ?>
+            <div class="job-exec-time"
+                 title="<?= Yii::t('queue-monitor/default', 'Last execute time and memory usage.') ?>">
+                <?= Yii::t('queue-monitor/default', 'Exec') ?>
+                : <?= $format->asInteger($model->lastExec->getDuration()) ?>s
+                <?php if ($model->lastExec->memory_usage){ ?>
                     / <?= $format->asShortSize($model->lastExec->memory_usage, 0) ?>
-                <?php endif ?>
+                <?php } ?>
             </div>
-        <?php endif ?>
+        <?php } ?>
     </div>
     <div class="job-class">
         <?= $format->asText($model->job_class) ?>
     </div>
     <div class="job-params">
-        <?php foreach ($model->getJobParams() as $property => $value): ?>
+        <?php foreach ($model->getJobParams() as $property => $value){ ?>
             <span class="job-param">
             <span class="job-param-name"><?= $format->asText($property) ?> =</span>
-            <span class="job-param-value"><?= htmlspecialchars(VarDumper::dumpAsString($value), ENT_QUOTES|ENT_SUBSTITUTE, Yii::$app->charset, true) ?></span>
+            <span class="job-param-value"><?= htmlspecialchars(VarDumper::dumpAsString($value), ENT_QUOTES | ENT_SUBSTITUTE, Yii::$app->charset, true) ?></span>
         </span>
-        <?php endforeach ?>
+        <?php } ?>
     </div>
-    <?php if ($model->lastExec && $model->lastExec->isFailed()): ?>
+    <?php if ($model->lastExec && $model->lastExec->isFailed()){ ?>
         <div class="job-error text-danger">
             <strong><?= Yii::t('queue-monitor/default', 'Error') ?>:</strong>
             <?= $format->asText($model->lastExec->getErrorMessage()) ?>
         </div>
-    <?php endif ?>
+    <?php } ?>
     <div class="job-border <?= $statusClass ?>"></div>
 </div>
 
 
 <div class="card mb-2">
-    <div class="card-body pt-3 pr-3 pb-3 pl-4">
+    <div class="card-body pt-2 pr-2 pb-2 pl-3">
         <div class="d-flex justify-content-between align-items-center">
             <div>
-                <a href="<?= Url::to(['view', 'id' => $model->id]) ?>" data-pjax="0">
-                    <?= Yii::t('queue-monitor/default', '#{jobId} by {sender}', [
-                        'jobId' => $format->asText($model->job_uid),
-                        'sender' => $format->asText($model->sender_name)
-                    ]) ?>
-                </a>
-                <?php if ($model->parent): ?>
+                <strong>
+                    <a href="<?= Url::to(['view', 'id' => $model->id]) ?>" data-pjax="0">
+                        <?= Yii::t('queue-monitor/default', '#{jobId} by {sender}', [
+                            'jobId' => $format->asText($model->job_uid),
+                            'sender' => $format->asText($model->sender_name)
+                        ]) ?>
+                    </a>
+                </strong>
+                <?php if ($model->parent){ ?>
                     <?= Yii::t('queue-monitor/default', 'from') ?>
                     <a href="<?= Url::to(['view', 'id' => $model->parent->id]) ?>" data-pjax="0">
                         #<?= $format->asText($model->parent->job_uid) ?>
                     </a>
-                <?php endif ?>
-                <span class="ml-4">
+                <?php } ?>
+                <small class="ml-4">
                     <?= Yii::t('queue-monitor/default', 'Pushed') ?>: <?= $format->asDatetime($model->pushed_at) ?>
-                </span>
-                <span class="ml-4" title="<?= Yii::t('main', 'Time to reserve of the job.') ?>">
+                </small>
+                <small class="ml-4" title="<?= Yii::t('main', 'Time to reserve of the job.') ?>">
                     <?= Yii::t('queue-monitor/default', 'TTR') ?>: <?= $format->asInteger($model->ttr) ?>s
-                </span>
-                <span class="ml-4">
+                </small>
+                <small class="ml-4">
                     <?= Yii::t('queue-monitor/default', 'Delay') ?>: <?= $format->asInteger($model->delay) ?>s
-                </span>
-                <span class="ml-4" title="<?= Yii::t('queue-monitor/default', 'Number of attempts.') ?>">
-                    <?= Yii::t('queue-monitor/default', 'Attempts') ?>: <?= $format->asInteger($model->getAttemptCount()) ?>
-                </span>
-                <span class="ml-4" title="<?= Yii::t('queue-monitor/default', 'Waiting time from push till first execute.') ?>">
+                </small>
+                <small class="ml-4" title="<?= Yii::t('queue-monitor/default', 'Number of attempts.') ?>">
+                    <?= Yii::t('queue-monitor/default', 'Attempts') ?>
+                    : <?= $format->asInteger($model->getAttemptCount()) ?>
+                </small>
+                <small class="ml-4"
+                       title="<?= Yii::t('queue-monitor/default', 'Waiting time from push till first execute.') ?>">
                     <?= Yii::t('queue-monitor/default', 'Wait') ?>: <?= $format->asInteger($model->getWaitTime()) ?>s
-                </span>
-                <?php if ($model->lastExec): ?>
-                    <span class="ml-4" title="<?= Yii::t('queue-monitor/default', 'Last execute time and memory usage.') ?>">
-                        <?= Yii::t('queue-monitor/default', 'Exec') ?>: <?= $format->asInteger($model->lastExec->getDuration()) ?>s
-                        <?php if ($model->lastExec->memory_usage): ?>
+                </small>
+                <?php if ($model->lastExec) { ?>
+                    <small class="ml-4"
+                           title="<?= Yii::t('queue-monitor/default', 'Last execute time and memory usage.') ?>">
+                        <?= Yii::t('queue-monitor/default', 'Exec') ?>
+                        : <?= $format->asInteger($model->lastExec->getDuration()) ?>s
+                        <?php if ($model->lastExec->memory_usage) { ?>
                             / <?= $format->asShortSize($model->lastExec->memory_usage, 0) ?>
-                        <?php endif ?>
-                    </span>
-                <?php endif ?>
+                        <?php } ?>
+                    </small>
+                <?php } ?>
             </div>
-            <div class="job-status label pl-2 pr-2 <?= $statusClass ?>"><?= $format->asText($model->getStatusLabel($status)) ?></div>
+            <div class="pl-2 pr-2 badge badge-<?= $statusClass ?> text-uppercase"><?= $format->asText($model->getStatusLabel($status)) ?></div>
         </div>
-
-
-    <div class="job-class">
-        <?= $format->asText($model->job_class) ?>
-    </div>
-    <div class="job-params">
-        <?php foreach ($model->getJobParams() as $property => $value): ?>
-            <span class="job-param">
-            <span class="job-param-name"><?= $format->asText($property) ?> =</span>
-            <span class="job-param-value"><?= htmlspecialchars(VarDumper::dumpAsString($value), ENT_QUOTES|ENT_SUBSTITUTE, Yii::$app->charset, true) ?></span>
-        </span>
-        <?php endforeach ?>
-    </div>
-    <?php if ($model->lastExec && $model->lastExec->isFailed()): ?>
-        <div class="job-error text-danger">
-            <strong><?= Yii::t('queue-monitor/default', 'Error') ?>:</strong>
-            <?= $format->asText($model->lastExec->getErrorMessage()) ?>
+        <h6 class="mt-2">
+            <strong><?= $format->asText($model->job_class) ?></strong>
+        </h6>
+        <div class="job-params">
+            <?php foreach ($model->getJobParams() as $property => $value) { ?>
+                <span class="job-param">
+                    <span class="job-param-name"><?= $format->asText($property) ?> =</span>
+                    <span class="job-param-value"><?= htmlspecialchars(VarDumper::dumpAsString($value), ENT_QUOTES | ENT_SUBSTITUTE, Yii::$app->charset, true) ?></span>
+                </span>
+            <?php } ?>
         </div>
-    <?php endif ?>
-    <div class="job-border <?= $statusClass ?>"></div>
+        <?php if ($model->lastExec && $model->lastExec->isFailed()) { ?>
+            <div class="job-error text-danger">
+                <strong><?= Yii::t('queue-monitor/default', 'Error') ?>:</strong>
+                <?= $format->asText($model->lastExec->getErrorMessage()) ?>
+            </div>
+        <?php } ?>
+        <div class="job-border bg-<?= $statusClass ?>"></div>
     </div>
 </div>
