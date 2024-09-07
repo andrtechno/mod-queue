e73ba41757e02e556181f69bc953363a4554aee1
Andrew <andrew.panix@gmail.com>
2024-09-07 14:16:19 +0300

    init

--- views/admin/job/_index-item.php
+++ views/admin/job/_index-item.php
@@ -31,69 +31,6 @@ switch ($status) {
         $statusClass = 'success';
 }
 ?>
-<div class="job-item d-none">
-    <div class="job-status"><?= $format->asText($model->getStatusLabel($status)) ?></div>
-    <div class="job-details">
-        <div class="job-push-uid">
-            <a href="<?= Url::to(['view', 'id' => $model->id]) ?>" data-pjax="0">
-                <?= Yii::t('queue-monitor/default', '#{jobId} by {sender}', [
-                    'jobId' => $format->asText($model->job_uid),
-                    'sender' => $format->asText($model->sender_name)
-                ]) ?>
-            </a>
-            <?php if ($model->parent){ ?>
-                <?= Yii::t('queue-monitor/default', 'from') ?>
-                <a href="<?= Url::to(['view', 'id' => $model->parent->id]) ?>" data-pjax="0">
-                    #<?= $format->asText($model->parent->job_uid) ?>
-                </a>
-            <?php } ?>
-        </div>
-        <div class="job-push-time">
-            <?= Yii::t('queue-monitor/default', 'Pushed') ?>: <?= $format->asDatetime($model->pushed_at) ?>
-        </div>
-        <div class="job-push-ttr" title="<?= Yii::t('main', 'Time to reserve of the job.') ?>">
-            <?= Yii::t('queue-monitor/default', 'TTR') ?>: <?= $format->asInteger($model->ttr) ?>s
-        </div>
-        <div class="job-push-delay">
-            <?= Yii::t('queue-monitor/default', 'Delay') ?>: <?= $format->asInteger($model->delay) ?>s
-        </div>
-        <div class="job-exec-attempts" title="<?= Yii::t('queue-monitor/default', 'Number of attempts.') ?>">
-            <?= Yii::t('queue-monitor/default', 'Attempts') ?>: <?= $format->asInteger($model->getAttemptCount()) ?>
-        </div>
-        <div class="job-exec-wait-time"
-             title="<?= Yii::t('queue-monitor/default', 'Waiting time from push till first execute.') ?>">
-            <?= Yii::t('queue-monitor/default', 'Wait') ?>: <?= $format->asInteger($model->getWaitTime()) ?>s
-        </div>
-        <?php if ($model->lastExec){ ?>
-            <div class="job-exec-time"
-                 title="<?= Yii::t('queue-monitor/default', 'Last execute time and memory usage.') ?>">
-                <?= Yii::t('queue-monitor/default', 'Exec') ?>
-                : <?= $format->asInteger($model->lastExec->getDuration()) ?>s
-                <?php if ($model->lastExec->memory_usage){ ?>
-                    / <?= $format->asShortSize($model->lastExec->memory_usage, 0) ?>
-                <?php } ?>
-            </div>
-        <?php } ?>
-    </div>
-    <div class="job-class">
-        <?= $format->asText($model->job_class) ?>
-    </div>
-    <div class="job-params">
-        <?php foreach ($model->getJobParams() as $property => $value){ ?>
-            <span class="job-param">
-            <span class="job-param-name"><?= $format->asText($property) ?> =</span>
-            <span class="job-param-value"><?= htmlspecialchars(VarDumper::dumpAsString($value), ENT_QUOTES | ENT_SUBSTITUTE, Yii::$app->charset, true) ?></span>
-        </span>
-        <?php } ?>
-    </div>
-    <?php if ($model->lastExec && $model->lastExec->isFailed()){ ?>
-        <div class="job-error text-danger">
-            <strong><?= Yii::t('queue-monitor/default', 'Error') ?>:</strong>
-            <?= $format->asText($model->lastExec->getErrorMessage()) ?>
-        </div>
-    <?php } ?>
-    <div class="job-border <?= $statusClass ?>"></div>
-</div>
 
 
 <div class="card mb-2">
@@ -148,10 +85,12 @@ switch ($status) {
             <strong><?= $format->asText($model->job_class) ?></strong>
         </h6>
         <div class="job-params">
-            <?php foreach ($model->getJobParams() as $property => $value) { ?>
+            <?php foreach ($model->getJobParams() as $property => $value) {
+                $val = htmlspecialchars(VarDumper::dumpAsString($value), ENT_QUOTES | ENT_SUBSTITUTE, Yii::$app->charset, true);
+                ?>
                 <span class="job-param">
                     <span class="job-param-name"><?= $format->asText($property) ?> =</span>
-                    <span class="job-param-value"><?= htmlspecialchars(VarDumper::dumpAsString($value), ENT_QUOTES | ENT_SUBSTITUTE, Yii::$app->charset, true) ?></span>
+                    <span class="job-param-value"><?= $val ?></span>
                 </span>
             <?php } ?>
         </div>
